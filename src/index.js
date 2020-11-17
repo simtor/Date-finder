8e32a3dffdc8be60296002e2ca5f37884f724df1
simtor <simone.ram@ada.ac.uk>
2020-11-17 16:04:51 +0000

    Added working function

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const rl = readline.createInterface({
   output : process.stdout
 });
 
+// var weekdays = [{0:'Sunday', 1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday'}]
+var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 
 rl.question("Please input the date: ", dateHandeler)
 function dateHandeler(dateInput){
     console.log(dateInput);
+    const day1 = new Date (dateInput);
+    const day2 = day1.getDay();
+    console.log(day2);
+    if (day2===1){
+      console.log('Monday')
+
+    }
+    console.log(weekdays[day2]);
+    // var weekday= weekdays.;
+    // console.log(weekday);
     if(dateInput==='Q'||'q'){
         rl.close()
     }
